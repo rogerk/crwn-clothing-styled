15dda5b1794878f9ebb25fbf3f301a38d0ab27e9
rogerk <rkitain@yahoo.com>
2021-04-29 08:51:46 -0400

    sign in / sign up cleanup

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -23,7 +23,6 @@ class SignUp extends React.Component {
     event.preventDefault();
 
     const { displayName, email, password, confirmPassword } = this.state;
-    console.log(`${email} ${password}`);
     if (password !== confirmPassword) {
       alert("passwords don't match");
       return;
